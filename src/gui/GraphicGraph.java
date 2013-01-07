94d788560655efee01c8be0a53452e1d34f30fc6
Benjamin ZIGH <Beben@Beben-PC>
2013-01-07 21:41:55 +0100

    Premier Commit foiré?

---
+++ src/gui/GraphicGraph.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.JComponent;
+
+public class GraphicGraph extends JComponent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4583711502138269427L;
+
+}
---
+++ src/model/Colored.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.awt.Color;
+
+/**
+ * 
+ * Définit la notion d'objet coloriable, permettant de traiter indifféremment
+ * la coloration des arêtes et des noeuds du graphe. 
+ * 
+ * @cons
+ *     $POST$
+ *     getColor() == INITIAL_COLOR
+ *
+ */
+
+public interface Colored {
+	
+	/**
+	 * La couleur initiale de l'objet 
+	 */
+	Color INITIAL_COLOR = Color.BLACK;
+	
+	//REQUETES
+	/**
+	 * Retourne la couleur actuelle de l'objet
+	 */
+	Color getColor();
+	
+	//COMMANDES
+	/**
+	 *  Définit la couleur de l'objet.
+     * @pre
+     * 
+     *     c != null
+     * @post
+     *     getColor() == c
+	 */
+	Color setColor(Color c);
+
+}
